dca53937d446edbc23ecde3412d0fd75dc9f8ff4
Adewole <wolecharles@gmail.com>
2023-12-13 14:58:57 +0100

    Feat : minimal server configuration

--- server/main.go
+++ server/main.go
@@ -1,20 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	echolog "github.com/labstack/gommon/log"
+)
+
+var (
+	ServerConfigFile = "config.json"
+)
+
+type serverConfig struct {
+	Ip       string
+	Port     int
+	Protocol string
+}
+
+func CreateServerConfig() serverConfig {
+	currentWorkingDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filepath := filepath.Join(currentWorkingDirectory, ServerConfigFile)
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var config serverConfig
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
 
 func main() {
+	var err error
+	config := CreateServerConfig()
 
-	app.Echo.Logger.SetLevel(log.INFO)
+	e := echo.New()
+	e.Logger.SetLevel(echolog.INFO)
 
 	// Define a route
-	app.Echo.GET("/", func(c echo.Context) error {
+	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
 	// Start server
 	go func() {
-		if err := app.Echo.Start(":8185"); err != nil && err != http.ErrServerClosed {
-			app.Echo.Logger.Fatal("shutting down the server")
+		address := fmt.Sprintf(":%d", config.Port)
+		if err := e.Start(address); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
 		}
 	}()
 
@@ -24,7 +71,7 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if err := app.Echo.Shutdown(ctx); err != nil {
-		app.Echo.Logger.Fatal(err)
+	if err = e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
 	}
 }
